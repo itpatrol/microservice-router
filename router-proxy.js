98af145a49eac1c49859ef12d4283d73ca88f152
Gor Martsen <Gor.martsen@gmail.com>
2016-08-03 16:10:52 -0400

    Fix SEARCH and POST
    Use variable stored routes to speedup processing

--- router-proxy.js
+++ router-proxy.js
@@ -22,6 +22,19 @@ var mControlCluster = new Cluster({
   }
 });
 
+if(!mControlCluster.isMaster) {
+  var routes = [];
+  updateRouteVariable(routes);
+  var interval = 6000;
+  if (process.env.INTERVAL) {
+    interval = process.env.INTERVAL;
+  }
+  setInterval(function(){
+//    console.log('Current routes');
+//    console.log(routes);
+    updateRouteVariable(routes)}, interval);
+}
+
 /**
  * Proxy GET requests.
  */
@@ -88,7 +101,7 @@ function ProxyRequestPOST(jsonData, requestDetails, callback) {
     }
 
     request({
-      uri: router.url + url[url.length - 1],
+      uri: router.url,
       method: 'POST',
       headers: requestDetails.headers,
       json: true,
@@ -215,7 +228,7 @@ function ProxyRequestSEARCH(jsonData, requestDetails, callback) {
     }
 
     request({
-      uri: router.url + url[url.length - 1],
+      uri: router.url,
       method: 'SEARCH',
       headers: requestDetails.headers,
       json: true,
@@ -237,37 +250,98 @@ function ProxyRequestSEARCH(jsonData, requestDetails, callback) {
  * Finx target URL.
  */
 function FindTarget(route, callback) {
+  console.log('Find route %s', route);
+  console.log(routes);
+  
+  var availableRoutes = [];
+  for (var i in routes) {
+    if (routes[i].path == route) {
+      availableRoutes.push(routes[i])
+    }
+  }
+  console.log('Available routes for %s', route);
+  console.log(availableRoutes);
+  if (availableRoutes.length == 0) {
+    return callback(new Error('Not found'), null);
+  }
+  if (availableRoutes.length == 1) {
+    return callback(null, availableRoutes.pop());
+  }
+
+  var random = Math.floor(Math.random() * (availableRoutes.length) + 1) - 1;
+  return callback(null, availableRoutes[random]);
+  /*  MongoClient.connect(process.env.MONGO_URL, function(err, db) {
+      if (err) {
+        return callback(err, null);
+      }
+
+      var collection = db.collection(process.env.MONGO_TABLE);
+      var query = {
+        path: route,
+        type: 'master'
+      };
+      collection.find(query).toArray(function(err, results) {
+        if (err) {
+          return callback(err, null);
+        }
+        if (!results || results.length == 0) {
+
+          // If it is not default, search for default for not found.
+          if( route != 'default' ) {
+            return FindTarget('default', callback);
+          }
+
+          return callback(new Error('Not found'), null);
+        }
+
+        // If we have only one route, do not do random search.
+        if(results.length == 1) {
+          return callback(null, results.pop());
+        }
+
+        var random = Math.floor(Math.random() * (results.length) + 1) - 1;
+        return callback(null, results[random]);
+      });
+    });*/
+}
+
+/**
+ * Update route infor each 10 sec.
+ */
+function updateRouteVariable() {
+//  console.log('Update routes');
+//  console.log(routes);
   MongoClient.connect(process.env.MONGO_URL, function(err, db) {
     if (err) {
-      return callback(err, null);
+      // If error, do nothing.
+      console.log(err);
+      return;
     }
 
     var collection = db.collection(process.env.MONGO_TABLE);
     var query = {
-      path: route,
       type: 'master'
     };
     collection.find(query).toArray(function(err, results) {
       if (err) {
-        return callback(err, null);
+        // If error, do nothing.
+        console.log(err);
+        return;
       }
       if (!results || results.length == 0) {
-
-        // If it is not default, search for default for not found.
-        if( route != 'default' ) {
-          return FindTarget('default', callback);
+        // If there is no results, do nothing
+        return;
+      }
+      var newRoutes = [];
+      for (var i in results) {
+        // get only changed in 60 sec.
+        if (results[i].changed > Date.now() - 60 * 1000) {
+          newRoutes.push(results[i]);
         }
-
-        return callback(new Error('Not found'), null);
       }
-
-      // If we have only one route, do not do random search.
-      if(results.length == 1) {
-        return callback(null, results.pop());
-      }
-
-      var random = Math.floor(Math.random() * (results.length) + 1) - 1;
-      return callback(null, results[random]);
+      routes = newRoutes;
+//      console.log('Routes updated');
+//      console.log(routes);
     });
   });
 }
