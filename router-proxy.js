3fcc5dd5fb659e3bf86112688f0901c9e3e5ed42
Gor Martsen <Gor.martsen@gmail.com>
2016-08-05 13:48:48 -0400

    Fix code style

--- router-proxy.js
+++ router-proxy.js
@@ -181,15 +181,15 @@ function proxyRequest(route, path, method, jsonData, requestDetails, callback) {
       if (error) {
         return proxyRequest(route, path, method, jsonData, requestDetails, callback);
       }
-      if(body.id) {
+      if (body.id) {
         body.url = process.env.BASE_URL + router.url + '/' + body.id;
       }
-      if(body._id) {
+      if (body._id) {
         body.url = process.env.BASE_URL + router.url + '/' + body._id;
       }
-      if( body instanceof Array){
+      if (body instanceof Array) {
         for (var i in body) {
-          if(body[i]._id) {
+          if (body[i]._id) {
             body[i].url = process.env.BASE_URL + router.url  + '/' + body[i]._id;
           }
         }
