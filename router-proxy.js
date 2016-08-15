dbcb907df5a46be7d96e24ca3a077516f956cc9f
Gor Martsen <Gor.martsen@gmail.com>
2016-08-15 14:52:40 -0400

    Copy content length as well

--- router-proxy.js
+++ router-proxy.js
@@ -175,7 +175,7 @@ function proxyRequest(route, path, method, jsonData, requestDetails, callback) {
 
     var headers = {};
     for (var i in requestDetails.headers) {
-      if (i != 'host' && i != 'content-length') {
+      if (i != 'host' ) {
         headers[i] = requestDetails.headers[i];
       }
     }
