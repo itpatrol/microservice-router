9b970b1c3d52a8031002afb6fd5f61d59e2413a7
Gor Martsen <Gor.martsen@gmail.com>
2017-05-08 22:28:44 -0400

    Codestyle clean up

--- includes/explorerClass.js
+++ includes/explorerClass.js
@@ -64,10 +64,12 @@ function ExplorerClass(requestDetails, callback) {
       }
     }
     resultMap.sort(function(a,b) {
-      if (a.path < b.path)
+      if (a.path < b.path) {
         return -1;
-      if (a.path > b.path)
+      }
+      if (a.path > b.path) {
         return 1;
+      }
       return 0;
     });
     if (self.mode == 'json') {
